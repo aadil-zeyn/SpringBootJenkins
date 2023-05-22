d8ba5f0a98123eab2b6cd4320d05c66ce089ff4c
Aadil <aadil@gamil.com>
2023-05-22 12:58:51 +0530

    k

--- src/main/java/com/UST/demo/Controller/Controller.java
+++ src/main/java/com/UST/demo/Controller/Controller.java
@@ -1,4 +1,19 @@
 package com.UST.demo.Controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
 public class Controller {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "welcome Aadil";
+    }
+    @GetMapping("/aadil")
+    public String welcomeAadil(){
+        return "welcome Aadil A A";
+    }
 }
